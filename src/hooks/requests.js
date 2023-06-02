e72751caec495885e9ce33a8eac673cff637b089
nikavella <nikavella22@gmail.com>
2023-06-02 19:25:05 +0600

    initial commit

---
+++ src/hooks/requests.js
@@ -0,0 +1,49 @@
+const API_URL = process.env.REACT_APP_URL;
+
+async function httpGetPlanets() {
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json( )
+  // Load planets and return as JSON.
+}
+
+async function httpGetLaunches() {
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b)=> a.flightNumber - b.flightNumber);
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  try{
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(launch )
+    });
+  }catch{
+    return {
+      ok: false
+    }
+  }
+}
+
+async function httpAbortLaunch(id) {
+  try{
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  }catch{
+    return {
+      ok: false
+    }
+  }
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
